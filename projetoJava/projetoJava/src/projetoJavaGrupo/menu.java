19aa6aac11edfb45c43155b1308651dd51d61a97
Erenilda Tavares <erenyldajsilva@gmail.com>
2023-03-27 20:38:10 -0300

    Atualizando projeto case1

--- projetoJava/projetoJava/src/projetoJavaGrupo/menu.java
+++ projetoJava/projetoJava/src/projetoJavaGrupo/menu.java
@@ -1,12 +1,14 @@
 package projetoJavaGrupo;
-
 import java.util.Scanner;
 
 public class menu {
 	public static void main(String[] args) {
 		Scanner leia = new Scanner(System.in);
+				String [] Produtos = {"Cesta Básica ", "Agasalho ", "Agua "};
+				int [] quantidade = {10, 10,10};
+				int idProduto=0, quant=0, opcao=0;
 
-		int opcao;
+		
 		String nome, tel, doc;
 
 		System.out.println("-----------------------------------------------------");
@@ -31,19 +33,43 @@ public class menu {
 		System.out.println("            1 - OFERECER DOAÇÃO.                     ");
 		System.out.println("            2 - SOLICITAR DOAÇÃO.                    ");
 		System.out.println("            3 - INFORMAÇÕES ÚTEIS.                   ");
-		System.out.println("            4 - ENCERRAR.                            ");
+		System.out.println("            4 - CADASTRO DE ONGS.                    ");
+		System.out.println("            5 - ENCERRAR.                            ");
 		System.out.println("-----------------------------------------------------");
 		System.out.println("                                                     ");
 		System.out.println("            Digite a opção desejada:                 ");
 		System.out.println("-----------------------------------------------------");
+		
 
 		opcao = leia.nextInt();
+		if(opcao==5){
+			System.out.println("Programa Encerrado!");
+			leia.close();
+			System.exit(5);
+		}
 
 		switch (opcao) {
 			case 1:
 				System.out.println("1 - OFERECER DOAÇÃO.");
 				System.out.println("\nFicamos felizes com sua atitude, ela pode salvar vidas.");
 				System.out.println("\nPor favor, selecione o produto que deseja doar");
+				
+				for(int i =0; i<3; i++){
+					System.out.println((i+1)+" "+ Produtos[i]);
+				}
+				while(idProduto<=0 || idProduto>3){
+			
+					System.out.println("selecione o produto que deseja doar: ");
+					idProduto=leia.nextInt();
+				}
+				
+				do{
+					System.out.println("selecione a quantidade de produtos deseja doar: ");
+					quant=leia.nextInt();
+					quantidade[idProduto - 1] += quant;
+				
+			
+				}while(quant<1);
 
 				break;
 			case 2:
@@ -54,7 +80,12 @@ public class menu {
 				System.out.println("3 - INFORMAÇÕES ÚTEIS.");
 
 				break;
-			case 4:
+
+				case 4:
+				System.out.println("3 - INFORMAÇÕES ÚTEIS.");
+
+				break;
+			case 5:
 				System.out.println("\nSoftware finalizado.");
 				leia.close();
 				System.exit(4);
